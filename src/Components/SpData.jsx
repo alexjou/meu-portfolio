2abd3a25a1df95a226cd49d7d426492e4443d133
Álex Silva <alex.silva@softwrap.com.br>
2021-04-04 12:39:15 -0300

    Add project

--- src/Components/SpData.jsx
+++ src/Components/SpData.jsx
@@ -1,28 +1,38 @@
-import Portfolio from './img/Portfolio.jpg'
-import MeuDinDin from './img/meudindin.png'
-import Doclist from './img/doclist.png'
+import Portfolio from "./img/Portfolio.jpg";
+import MeuDinDin from "./img/meudindin.png";
+import Doclist from "./img/doclist.png";
+import Climao from "./img/web.gif";
 
 const SpData = [
   {
-    title: 'Meu primeiro Portfolio',
-    desc: 'Desenvolvi em ReacJS, tanto para aprendizado quanto para exibir minhas habilidades.',
-    link: 'https://alexjou.github.io/portfolio',
+    title: "Meu primeiro Portfolio",
+    desc:
+      "Desenvolvi em ReacJS, tanto para aprendizado quanto para exibir minhas habilidades.",
+    link: "https://alexjou.github.io/portfolio",
     src: Portfolio,
-    animate: 'fade-right',
+    animate: "fade-right",
   },
   {
-    title: 'Doclist App',
-    desc: 'Aplicativo para administrar pacientes.',
-    link: '',
+    title: "Doclist App",
+    desc: "Aplicativo para administrar pacientes.",
+    link: "",
     src: Doclist,
-    animate: 'fade-left',
+    animate: "fade-left",
   },
   {
-    title: 'Meu DinDin App',
-    desc: 'Aplicativo para controle financeiro.',
-    link: 'http://bit.ly/MeuDinDin',
+    title: "Meu DinDin App",
+    desc: "Aplicativo para controle financeiro.",
+    link: "http://bit.ly/MeuDinDin",
     src: MeuDinDin,
-    animate: 'fade-right',
+    animate: "fade-right",
   },
-]
-export default SpData
+  {
+    title: "Climão",
+    desc:
+      "Site responsivo que você pode ver os dados meteorológicos da sua localização ou de qualquer lugar do mundo.",
+    link: "https://alexjou.github.io/climao/",
+    src: Climao,
+    animate: "fade-right",
+  },
+];
+export default SpData;
